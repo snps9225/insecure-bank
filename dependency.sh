76140e9a21789c83a1fbf2627048427d66f91a81
snps9225 <56947899+snps9225@users.noreply.github.com>
2022-01-10 10:44:18 -0800

    Update dependency.sh

--- dependency.sh
+++ dependency.sh
@@ -6,4 +6,4 @@ rm -rf dependency-check-6.5.2-release
 curl -L https://github.com/jeremylong/DependencyCheck/releases/download/v6.5.2/dependency-check-6.5.2-release.zip > dependency-check.zip
 unzip dependency-check.zip
 
-./dependency-check.sh --format HTML --out . --scan **/*.jar
+./dependency-check/bin/dependency-check.sh --format HTML --out . --scan ./target/**/*.jar
